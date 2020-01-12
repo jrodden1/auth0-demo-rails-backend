4dcaf24976ecc394243e10785b465b420e680c44
Jeremiah Rodden <jrodden1@gmail.com>
2020-01-11 21:16:46 -0700

    temporary CORS config set in place

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,9 @@
 
 # Rails.application.config.middleware.insert_before 0, Rack::Cors do
 #   allow do
-#     origins 'example.com'
-#
+#     origins '*'
+#     # Leaving at all for now.  Will need to change when deployed to production
+
 #     resource '*',
 #       headers: :any,
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
